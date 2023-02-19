dfe224f0352b831bdf4f74c9db98b14303328ead
Stacey <stacey6105@gmail.com>
2023-02-19 10:06:26 +0000

    Test data set up

---
+++ db/data/test-data/index.js
@@ -0,0 +1,3 @@
+exports.professionalsTestData = require("./professionalsData");
+exports.moodTrackingTestData = require("./moodTrackingData");
+exports.usersTestData = require("./usersData");
---
+++ db/data/test-data/professionalsData.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {
+        fullName: "Lilliana Valentina",
+        email: "lilliana_valentina@plt.com",
+        registrationNumber: "CP871095",
+        availableHours: [{day: "monday", hours: [8, 14]}, {day: "tuesday", hours: [1, 8]}, {day: "wednesday", hours: [14, 21]}, {day: "thursday", hours: [8, 10]}, {day: "friday", hours: [11, 17]}, {day: "saturday", hours: [18, 20]}, {day: "sunday", hours: [10, 12]}],
+        avatarURL: "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+    {
+        fullName: "Soraya Malik",
+        email: "soymilk@oatly.com",
+        registrationNumber: "CP524891",
+        availableHours: [{day: "monday", hours: [7, 14]}, {day: "tuesday", hours: [6, 11]}, {day: "wednesday", hours: [7, 12]}, {day: "thursday", hours: [20, 24]}, {day: "friday", hours: [13, 20]}, {day: "saturday", hours: [13, 22]}, {day: "sunday", hours: [10, 13]}],
+        avatarURL: "https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+    {
+        fullName: "Dashiel Fitzpatrick",
+        email: "dashiel1hr8rn@skymail.com",
+        registrationNumber: "CP627540",
+        availableHours: [{day: "monday", hours: [9, 17]}, {day: "tuesday", hours: [9, 17]}, {day: "wednesday", hours: [9, 17]}, {day: "thursday", hours: [9, 17]}, {day: "friday", hours: [9, 17]}, {day: "saturday", hours: [9, 17]}, {day: "sunday", hours: [9, 17]}],
+        avatarURL: "https://images.pexels.com/photos/1722198/pexels-photo-1722198.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+    {
+        fullName: "Ronin Flynn",
+        email: "flynn@ronin.org",
+        registrationNumber: "CP653218",
+        availableHours: [{day: "monday", hours: [13, 20]}, {day: "tuesday", hours: [13, 20]}, {day: "wednesday", hours: [13, 20]}, {day: "thursday", hours: [13, 20]}, {day: "friday", hours: [13, 20]}, {day: "saturday", hours: [13, 20]}, {day: "sunday", hours: [13, 20]}],
+        avatarURL: "https://images.pexels.com/photos/12437056/pexels-photo-12437056.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+    {
+        fullName: "Eleanor Shellstrop",
+        email: "eleanor@yabasic.com",
+        registrationNumber: "CP417860",
+        availableHours: [{day: "monday", hours: [13, 20]}, {day: "tuesday", hours: [13, 20]}, {day: "wednesday", hours: [13, 20]}, {day: "thursday", hours: [13, 20]}, {day: "friday", hours: [13, 20]}, {day: "saturday", hours: [13, 20]}, {day: "sunday", hours: [13, 20]}],
+        avatarURL: "https://static.wikia.nocookie.net/thegoodplace/images/9/9e/4eleanor.jpg/revision/latest?cb=20190914140736"
+    }
+]
