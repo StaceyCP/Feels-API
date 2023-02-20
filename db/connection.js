d423bda4666e4e91e0fef45fe635f6932b9c636a
Stacey <stacey6105@gmail.com>
2023-02-20 09:53:19 +0000

    db connections set up

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect('mongodb://127.0.0.1:27017/feels-data')
+    .then(() => {
+        console.log("Connected to Local DB");
+    })
+    .catch((err) => {
+        console.log("Oops" + err)
+    })
+
+const db = mongoose.createConnection()
+
+module.exports = { db };
