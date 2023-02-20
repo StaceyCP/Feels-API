7689f4fc5197ab785b80aafb8b07d66bd185f66f
LukeWatson24 <watsonluke@sky.com>
2023-02-20 14:11:01 +0000

    async-await

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -1,23 +1,14 @@
 const { db } = require("../connection");
 const Professional = require("../profesionalSchema");
-const professionalsData = require("../data/test-data/professionalsData");
 
-function seedProfessionals(data) {
-  return Professional.deleteMany({})
-    .then(() => {
-      console.log("Droped collection");
-      return Professional.createCollection();
-    })
-    .then(() => {
-      console.log("Collection created");
-      return Professional.insertMany(data);
-    })
-    .then(() => {
-      console.log("Data inserted");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+async function seedProfessionals(data) {
+  try {
+    await Professional.deleteMany({});
+    await Professional.createCollection();
+    await Professional.insertMany(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = seedProfessionals;
