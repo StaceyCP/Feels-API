76c033583e404d6e5683558ff7735b3593a88a16
siandaniel <siandaniel@ymail.com>
2023-02-20 14:05:44 +0000

    set up seed file

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -1,2 +1,19 @@
-const {db} = require('../connection');
+const { db } = require("../connection");
+const User = require("../schemas/userSchema");
+const mongoose = require("mongoose");
 
+const seed = ({ usersData }) => {
+  return User.deleteMany({})
+    .then(() => {
+      console.log("User collection created");
+      return User.insertMany(usersData);
+    })
+    .then(() => {
+      console.log("Users data inserted");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = seed;
