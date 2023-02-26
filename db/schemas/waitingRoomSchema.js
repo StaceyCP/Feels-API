43fa310ff4e5271b15bd4496dc81e2275dfec07d
Stacey <stacey6105@gmail.com>
2023-02-26 23:21:15 +0000

    Set up waiting room schema

---
+++ db/schemas/waitingRoomSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const waitingRoomSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    avatar_url: { type: String, required: true },
+    chatTopics: { type: String },
+  },
+  { timestamps: true }
+);
+
+const WaitingRoom = mongoose.model("WaitingRoom", waitingRoomSchema);
+
+module.exports = WaitingRoom;
