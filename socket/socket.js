f5bbed5b338d817114d5cb3db77bad3ec2e545dc
Stacey <stacey6105@gmail.com>
2023-02-28 16:30:46 +0000

    Initialise chat room persistance

--- socket/socket.js
+++ socket/socket.js
@@ -10,6 +10,23 @@ app.get("/", (req, res) => {
 });
 
 io.use((socket, next) => {
+  const sessionID = socket.handshake.auth.sessionID;
+  if (sessionID) {
+    const session = sessionStore.findSession(sessionID);
+    if (session) {
+      socket.sessionID = sessionID;
+      if (!session.username) {
+        socket.isProfessional = true;
+        socket.fullName = session.fullName;
+      } else if (!session.fullName) {
+        socket.isProfessional = false;
+        socket.isWaiting = false;
+        socket.username = session.username;
+      }
+      socket.connectionID = session.connectionID;
+      next();
+    }
+  }
   const username = socket.handshake.auth.username;
   const fullName = socket.handshake.auth.fullName;
   if (!username) {
@@ -20,11 +37,13 @@ io.use((socket, next) => {
     socket.isWaiting = false;
     socket.username = username;
   }
+  socket.sessionID = Math.floor(Math.random() * 1000000000000000);
+  socket.connectionID = Math.floor(Math.random() * 1000000000000000);
   next();
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("Successful connection");
   let users = [];
   for (let [id, socket] of io.of("/").sockets) {
     if (socket.isProfessional) {
@@ -32,14 +51,15 @@ io.on("connection", (socket) => {
     }
     users.push(id);
   }
-  console.log(socket.id);
+
   socket.on("message", ({ message, to }) => {
     io.to(to).emit("message", { message, from: socket.id });
   });
+
   socket.on("waiting", () => {
     socket.isWaiting = true;
-    console.log(socket.isWaiting);
   });
+
   socket.on("refresh", () => {
     const newUsers = [];
     for (let [id, socket] of io.of("/").sockets) {
@@ -48,6 +68,7 @@ io.on("connection", (socket) => {
     }
     socket.emit("users", newUsers);
   });
+
   socket.emit("users", users);
 });
 
