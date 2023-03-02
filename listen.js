8528f3403ddeca841b7c529d885f8dc684e79884
Luke <WATSONLUKE@sky.com>
2023-03-02 09:34:52 +0000

    changed listen port

--- listen.js
+++ listen.js
@@ -4,6 +4,6 @@ const { server } = require("./socket/socket");
 const { PORT = 9090 } = process.env;
 
 // app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
-server.listen({ host: "192.168.0.23", port: PORT }, () => {
+server.listen(PORT, () => {
   console.log(`listening on port WS ${PORT}`);
 });
